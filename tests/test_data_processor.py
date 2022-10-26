eedc983a2541ad52caa87d16770da065521d6e63
Daphne <daqu2831@colorado.edu>
2022-10-26 17:03:06 +0000

    Write test for type of input

--- tests/test_data_processor.py
+++ tests/test_data_processor.py
@@ -12,3 +12,10 @@ class TestUtils(unittest.TestCase):
     def test_get_random_matrix(self):
         # Make sure matrix is correct shape
         self.assertEqual(dp.get_random_matrix(4, 2).shape, (4, 2))
+        
+        # Negative test
+        self.assertNotEqual(dp.get_random_matrix(4, 2).shape, (2, 4))
+        
+        # Make sure inputs are integers
+        with self.assertRaises(TypeError):
+            dp.get_random_matrix(3.5, 13)
