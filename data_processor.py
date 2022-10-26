f7cbb163f58cee2245cba0cb97a5bbbf9c9129a2
Daphne <daqu2831@colorado.edu>
2022-10-26 16:59:40 +0000

    Set up data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -1,4 +1,10 @@
-# remember to import your libraries!
+'''
+Functions to aid in data processing
+
+    * get_random_matrix : Creates an MxN matrix filled with random numbers
+    * get_file_dimensions :
+    * write_matrix_to_file : 
+'''
 
 def get_random_matrix(num_rows, num_columns):
 	return None
